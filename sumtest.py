02dc07a85d14aa7a5e7d59adeee9e9f65279f2ee
Xiangyida <mrxiangyd@outlook.com>
2018-12-23 16:41:35 +0800

    练习题：输入数据并求和

---
+++ sumtest.py
@@ -0,0 +1,5 @@
+a = int(input('please enter the first num: '))
+b = int(input('please enter the second num: '))
+c = int(input('please enter the third num: '))
+d = a+b+c
+print('a is {},b is {},c is {},a+b+c={}'.format(a,b,c,d))
