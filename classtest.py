f3e12f8e810b5b0190331d62238cfd1169e09611
Xiangyida <mrxiangyd@outlook.com>
2018-12-23 15:18:49 +0800

    修改classtest.py

--- classtest.py
+++ classtest.py
@@ -4,29 +4,37 @@ class UserData(object):
     def __init__(self, id, name):
         self.id = id
         self._name = name
+
     def __repr__(self):
         return 'ID:{} Name:{}'.format(self.id, self.name)
 
-class NewUser(UserData):
+
+class NewUser(object):
     group = 'shiyanlou-louplus'
-    def __init__(self,id,name):
+
+    def __init__(self, id, name):
         self.id = id
         self._name = name
+
     @property
     def name(self):
         return self._name
+
     @name.setter
     def name(self, value):
         if len(value) > 3:
             self._name = value
         else:
             print('Error')
+
     @classmethod
     def get_group(cls):
         return cls.group
+
     @staticmethod
     def format_userdata(id, name):
-        return "{}'s id is {}".format(name,id)
+        return "{}'s id is {}".format(name, id)
+
 
 if __name__ == '__main__':
     user1 = NewUser(101, 'Jack')
@@ -35,4 +43,3 @@ if __name__ == '__main__':
     user2 = NewUser(102, 'Louplus')
     print(user1.name)
     print(user2.name)
-
