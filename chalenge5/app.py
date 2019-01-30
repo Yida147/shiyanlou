a26f2919ca6372552fce420b8759d4a13ce56d55
Xiangyida <mrxiangyd@outlook.com>
2019-01-30 21:41:04 +0800

    修改环境

---
+++ chalenge5/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Founder123@localhost/shiyanlou'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+db = SQLAlchemy(app)
+
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    create_time = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category',backref=db.backref('categories',lazy='dynamic'))
+    content = db.Column(db.Text)
+    
+    def __repr__(self):
+        return '<File %r>' % self.title
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    def __repr__(self):
+        return '<Category %r>' % self.name
+
