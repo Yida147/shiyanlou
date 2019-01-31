2346f343a2d5945dc6e72b4e54c9e003be3bb634
Xiangyida <mrxiangyd@outlook.com>
2019-01-31 10:57:25 +0800

    修改环境

--- chalenge5/app.py
+++ chalenge5/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import json
+from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -25,4 +26,16 @@ class Category(db.Model):
     name = db.Column(db.String(80))
     def __repr__(self):
         return '<Category %r>' % self.name
+
+@app.route('/')
+def index():
+    # 显示文章名称的列表
+    # 页面中需要显示所有文章的标题（title）列表，此外每个标题都需要使用 `<a href=XXX></a>` 链接到对应的文章内容页面
+
+@app.route('/files/<file_id>')
+def file(file_id):
+    # file_id 为 File 表中的文章 ID
+    # 需要显示 file_id  对应的文章内容、创建时间及类别信息（需要显示类别名称）
+    # 如果指定 file_id 的文章不存在，则显示 404 错误页面
+
 
